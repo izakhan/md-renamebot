f1b13f286f7127237015feedee828b11bddef6d2
izakhan <121670888+izakhan@users.noreply.github.com>
2023-01-01 16:52:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,10 +18,10 @@ async def start(bot, message):
              [[
                InlineKeyboardButton("♦️ Help", callback_data = "help")
              ],[
-               InlineKeyboardButton('📢 Update Channel', url='https://t.me/GreyMatter_Bots'),
-               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/GreyMatters_Bots_Discussion')
+               InlineKeyboardButton('📢 Update Channel', url='https://t.me/thanimaiBots'),
+               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/thanimaibots')
              ],[
-                InlineKeyboardButton('📃 Source code', url='https://github.com/Greymattersbot/md-renamebot')
+                InlineKeyboardButton('📃 Source code', url='https://t.me/sarbudeen786')
              ]]
    ))
                             
@@ -66,9 +66,9 @@ async def cb_handler(client: Client , query: CallbackQuery):
                InlineKeyboardButton("♦️ Help", callback_data = "help")
              ],[
                InlineKeyboardButton('📢 Update Channel', url='https://t.me/GreyMatter_Bots'),
-               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/GreyMatters_Bots_Discussion')
+               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/thanimaibots ')
              ],[
-                InlineKeyboardButton('📃 Source code', url='https://github.com/Greymattersbot/md-renamebot')
+                InlineKeyboardButton('📃 Source code', url='https://t.me/sarbudeen786')
              ]]
         ))
         
@@ -99,7 +99,7 @@ async def cb_handler(client: Client , query: CallbackQuery):
             disable_web_page_preview = True,
             reply_markup=InlineKeyboardMarkup(
             [[
-              InlineKeyboardButton('📃 Source code', url='https://github.com/Greymattersbot/md-renamebot')
+              InlineKeyboardButton('📃 Source code', url='https://t.me/sarbudeen786')
             ],[
               InlineKeyboardButton('Back', callback_data = "help"),
             ]]
