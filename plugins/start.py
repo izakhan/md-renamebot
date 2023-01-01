54051e4e25fb69038a2ba074ae5edf60649e877a
izakhan <121670888+izakhan@users.noreply.github.com>
2023-01-01 17:00:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -66,7 +66,7 @@ async def cb_handler(client: Client , query: CallbackQuery):
                InlineKeyboardButton("♦️ Help", callback_data = "help")
              ],[
                InlineKeyboardButton('📢 Update Channel', url='https://t.me/thanimaibots'),
-               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/thanimaibots ')
+               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/thanimaibots')
              ],[
                 InlineKeyboardButton('📃 Source code', url='https://t.me/sarbudeen786')
              ]]
